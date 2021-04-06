6fc4b411324178b54ddc14af483e975ece7a3a52
Xinjian OUYANG <xinjian.ouyang@student-cs.fr>
2021-04-06 12:34:42 +0200

    update

--- ST7_models/utils.py
+++ ST7_models/utils.py
@@ -1,7 +1,17 @@
 from sklearn.metrics import accuracy_score
 import tensorflow as tf
 import numpy as np
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import tensorflow as tf
+from tensorflow.python.lib.io import file_io
+from keras.preprocessing.image import ImageDataGenerator
+
+import skimage
+from skimage.transform import rescale, resize
+
+
 # configure image data augmentation
 datagen = ImageDataGenerator(horizontal_flip=True)
 
@@ -34,3 +44,24 @@ def tta_evaluate_model(model, testX, testY):
     testY_labels = np.argmax(testY, axis=1)
     acc = accuracy_score(testY_labels, yhats)
     return acc
+
+def get_data(dataset_path, Resize_pixelsize = 197):
+    
+    file_stream = file_io.FileIO(dataset_path, mode='r')
+    data = pd.read_csv(file_stream)
+    data['pixels'] = data['pixels'].apply(lambda x: [int(pixel) for pixel in x.split()])
+    X, Y = data['pixels'].tolist(), data['emotion'].values
+    X = np.array(X, dtype='float32').reshape(-1,48,48,1)
+    X = X/255.0
+   
+    X_res = np.zeros((X.shape[0], Resize_pixelsize,Resize_pixelsize,3))
+    for ind in range(X.shape[0]): 
+        sample = X[ind]
+        sample = sample.reshape(48, 48)
+        image_resized = resize(sample, (Resize_pixelsize, Resize_pixelsize), anti_aliasing=True)
+        X_res[ind,:,:,:] = image_resized.reshape(Resize_pixelsize,Resize_pixelsize,1)
+
+    Y_res = np.zeros((Y.size, 7))
+    Y_res[np.arange(Y.size),Y] = 1    
+    
+    return  X, X_res, Y_res
